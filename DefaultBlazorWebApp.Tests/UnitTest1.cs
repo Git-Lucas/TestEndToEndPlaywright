670f621de6d8ad764f39904269626627cf9f92be
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2024-09-09 19:39:13 -0300

    Add project files.

---
+++ DefaultBlazorWebApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DefaultBlazorWebApp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
